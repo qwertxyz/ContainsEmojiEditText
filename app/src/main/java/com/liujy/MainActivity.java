16083275f146a1fdc6e43ca9206b5f5885e9f084
Liujy <a584551855@163.com>
2015-04-30 09:50:16 +0800

    first add
    EditText过滤Emoji表情符

---
+++ app/src/main/java/com/liujy/MainActivity.java
@@ -0,0 +1,15 @@
+package com.liujy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
